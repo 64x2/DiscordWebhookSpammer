dd9f5ae8a89f9990b6a0f664a56b5ba62e3ba8d7
64x2 <79061462+64x2@users.noreply.github.com>
2022-05-14 12:18:48 +0100

    Added official URL print

--- WebhookSpammer.py
+++ WebhookSpammer.py
@@ -1,6 +1,10 @@
 from discord_webhook import DiscordWebhook
 import time
-url = input("Enter what webook to spam:\n> ")
+
+print("Webhook Spammer by xofo#1749, educational purposes only!")
+print("github.com/64x2/DiscordWebhookSpammer\n")
+
+url = input("Enter the webook you would like to spam:\n> ")
 value = input("Enter what you would like to send to the webook:\n> ")
 times = int(input("Enter the amount of times you would like to send the message:\n> "))
 delay = int(input("Enter delay between messages in milliseconds, reccomended to use 500ms or more:\n> "))
