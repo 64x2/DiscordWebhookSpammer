80b7379a68d672ccfe9991f26baab69fb030485f
64x2 <79061462+64x2@users.noreply.github.com>
2022-05-14 11:51:11 +0100

    Added rate limit checker

--- WebhookSpammer.py
+++ WebhookSpammer.py
@@ -6,6 +6,6 @@ times = int(input("Enter the amount of times you would like to send the message:
 delay = int(input("Enter delay between messages in milliseconds, reccomended to use 500ms or more:\n> "))
 
 for i in range(times):
-    webhook = DiscordWebhook(url=url, content=f"{value}")
+    webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=f"{value}")
     response = webhook.execute()
     time.sleep(delay/1000)
