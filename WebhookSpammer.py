e9d1060b3f17d9d526d7dc965833d0d309465fb6
64x2 <79061462+64x2@users.noreply.github.com>
2022-05-14 13:46:50 +0100

    Added loop after finished spamming
    Credits to @Invy55

--- WebhookSpammer.py
+++ WebhookSpammer.py
@@ -4,12 +4,24 @@ import time
 print("Webhook Spammer by xofo#1749, educational purposes only!")
 print("github.com/64x2/DiscordWebhookSpammer\n")
 
-url = input("Enter the webook you would like to spam:\n> ")
-value = input("Enter what you would like to send to the webook:\n> ")
-times = int(input("Enter the amount of times you would like to send the message:\n> "))
-delay = int(input("Enter delay between messages in milliseconds, reccomended to use 500ms or more:\n> "))
+def spammer():
+    url = input("Enter the webhook you would like to spam: \n> ")
+    value = input("Enter what you would like to send to the webook: \n> ")
+    times = int(input ("Enter the amount of times you would like to send the message: \n> "))
+    delay = int(input ("Enter delay between messages in milliseconds, recomended to use 500ms or more: \n> "))
 
-for i in range(times):
-    webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=f"{value}")
-    response = webhook.execute()
-    time.sleep(delay/1000)
+    for i in range (times):
+        webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=f"{value}")
+        response = webhook.execute()
+        time.sleep(delay/1000)
+
+if __name__ == "__main__": # Credit to @Invy55 for this area :^)
+    spammer()
+    while True:
+        try:
+            if "y" in input("Would you like to start over? (Y/N) \n").lower():
+                spammer()
+            else:
+                break
+        except KeyboardInterrupt:
+            break
