db1063849f02f074c61a0133e4330e17e7643527
64x2 / Xofo <79061462+64x2@users.noreply.github.com>
2022-05-29 19:30:35 +0100

    most likely the final update
    added cool ascii art and added input for custom webhook username

--- WebhookSpammer.py
+++ WebhookSpammer.py
@@ -1,17 +1,30 @@
 from discord_webhook import DiscordWebhook
 import time
+from colorama import Fore 
 
-print("Webhook Spammer by xofo#1749, educational purposes only!")
-print("github.com/64x2/DiscordWebhookSpammer\n")
+print(Fore.RED + " /$$      /$$           /$$       /$$                           /$$        /$$$$$$                                                                     ")
+print(Fore.RED + "| $$  /$ | $$          | $$      | $$                          | $$       /$$__  $$                                                                    ")
+print(Fore.RED + "| $$ /$$$| $$  /$$$$$$ | $$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$ | $$   /$$| $$  \__/  /$$$$$$   /$$$$$$  /$$$$$$/$$$$  /$$$$$$/$$$$   /$$$$$$   /$$$$$$ ")
+print(Fore.RED + "| $$/$$ $$ $$ /$$__  $$| $$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$  /$$/|  $$$$$$  /$$__  $$ |____  $$| $$_  $$_  $$| $$_  $$_  $$ /$$__  $$ /$$__  $$")
+print(Fore.RED + "| $$$$_  $$$$| $$$$$$$$| $$  \ $$| $$  \ $$| $$  \ $$| $$  \ $$| $$$$$$/  \____  $$| $$  \ $$  /$$$$$$$| $$ \ $$ \ $$| $$ \ $$ \ $$| $$$$$$$$| $$  \__/")
+print(Fore.RED + "| $$$/ \  $$$| $$_____/| $$  | $$| $$  | $$| $$  | $$| $$  | $$| $$_  $$  /$$  \ $$| $$  | $$ /$$__  $$| $$ | $$ | $$| $$ | $$ | $$| $$_____/| $$      ")
+print(Fore.RED + "| $$/   \  $$|  $$$$$$$| $$$$$$$/| $$  | $$|  $$$$$$/|  $$$$$$/| $$ \  $$|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$ | $$ | $$| $$ | $$ | $$|  $$$$$$$| $$      ")
+print(Fore.RED + "|__/     \__/ \_______/|_______/ |__/  |__/ \______/  \______/ |__/  \__/ \______/ | $$____/  \_______/|__/ |__/ |__/|__/ |__/ |__/ \_______/|__/      ")
+print(Fore.RED + "                                                                                   | $$                                                                ")
+print(Fore.RED + "                                                                                   | $$                                                                ")
+print(Fore.RED + "                                                                                   |__/                                                                ")
+print("")
+print(Fore.BLUE + "Welcome to Webhook Spammer! Made by Xofo @ github.com/64x2/DiscordWebhookSpammer for free use.")
 
 def spammer():
-    url = input("Enter the webhook you would like to spam: \n> ")
-    value = input("Enter what you would like to send to the webook: \n> ")
-    times = int(input ("Enter the amount of times you would like to send the message: \n> "))
-    delay = int(input ("Enter delay between messages in milliseconds, recomended to use 500ms or more: \n> "))
+    url = input(Fore.GREEN + "Enter the webhook you would like to spam: \n> ")
+    value = input(Fore.GREEN + "Enter what you would like to send to the webook: \n> ")
+    times = int(input (Fore.GREEN + "Enter the amount of times you would like to send the message: \n> "))
+    delay = int(input (Fore.GREEN + "Enter delay between messages in milliseconds, recomended to use 500ms or more: \n> "))
+    username = input(Fore.GREEN + "Set the username of the webhook: \n> ")
 
     for i in range (times):
-        webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=f"{value}")
+        webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=f"{value}", username=f"{username}")
         response = webhook.execute()
         time.sleep(delay/1000)
 
@@ -19,7 +32,7 @@ if __name__ == "__main__": # Credit to @Invy55 for this area :^)
     spammer()
     while True:
         try:
-            if "y" in input("Would you like to start over? (Y/N) \n").lower():
+            if "y" in input(Fore.GREEN + "Would you like to start over? (Y/N) \n").lower():
                 spammer()
             else:
                 break
